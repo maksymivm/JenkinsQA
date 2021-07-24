1e033f0c778c98db4213716fb39d1699526052d0
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2021-07-24 02:17:24 -0400

    project (alpha)

---
+++ selenium-grid/wait-for-jenkis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# wait-for-jenkins.sh
+
+set -e
+
+seconds=120
+while ! curl -sSL "http://localhost:8080/" 2>&1 \
+             | grep "<title>Sign in [Jenkins]</title>" >/dev/null; do
+    echo 'Waiting for the Jenkins'
+    sleep 10
+    seconds=$(($seconds - 10))
+
+    if [ $seconds -le 0 ]; then
+        echo "Time out"
+        exit 1
+    fi
+done
+
+echo "Jenkins is up"
